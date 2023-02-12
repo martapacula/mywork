a9672b5902fc878b28a84639d86ac81a12b0dfb9
Marta Pa <G00425668@atu.ie>
2023-02-12 20:06:53 +0000

    week2

---
+++ week2/bank01.py
@@ -0,0 +1,11 @@
+# bank01.py attempt
+# Author: Marta
+# add two variables
+
+
+x = int(input("Enter amount1(in cents): "))
+y = int(input("Enter amount2(in cents): "))
+ans = (x+y)/100
+print(f"The sum of these is €{ans}")
+
+
