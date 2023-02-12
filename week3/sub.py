2cdf20be7432c252f7739a5edf26dbb4a469db5b
Marta Pa <G00425668@atu.ie>
2023-02-12 14:11:03 +0000

    week3

---
+++ week3/sub.py
@@ -0,0 +1,8 @@
+# sub.py
+# Author Marta P
+# program to subtract
+
+x = int(input("Enter first number: "))
+y = int(input("Enter second number: "))
+answer=x-y
+print("{} minus {} is {}".format(x, y, answer))
