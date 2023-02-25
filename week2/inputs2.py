4260c889e9e141564695ea4071e81a1ed961d435
Marta Pa <G00425668@atu.ie>
2023-02-25 15:28:59 +0000

    week2

---
+++ week2/inputs2.py
@@ -0,0 +1,13 @@
+#inputs2.py
+#author: Marta
+#second glance on inputs
+
+name = input("Please enter your nick name: ")
+print(f"Your real name is {name}")
+age = int(input("Please provide your year of birth: "))
+age = 2023 - age
+print(f"Your age is {age} ")
+
+
+
+
