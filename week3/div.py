93b49bf7ef08898ed402db83c15d2cc72d88f307
Marta Pa <G00425668@atu.ie>
2023-02-26 15:51:49 +0000

    week3

---
+++ week3/div.py
@@ -0,0 +1,9 @@
+#div.py
+#Author: Marta
+
+x= int(input("Enter first number: "))
+y= int(input("Enter the number you want to divide by: "))
+ans= x//y 
+reminder =x%y 
+print(f"{x} divided by {y} is {ans} with reminder {reminder}")
+
---
+++ week3/pwd.py
@@ -0,0 +1,16 @@
+#testing from internet
+#author marta
+
+# The bank account number (example)
+bank_account = "1234567890"
+
+# Hide all but the last 4 digits
+hidden_account = "xxxxxx" + bank_account[-0]
+
+
+print("Your bank account number is:", hidden_account)
+
+
+
+
+
