94821fe55d4e11126a4060b44beeb0a25b253754
Marta Pa <G00425668@atu.ie>
2023-03-12 21:01:10 +0000

    week4

---
+++ week4/wajl.py
@@ -0,0 +1,16 @@
+#gpt
+#while num <= 5:
+   # print(num)
+   # num += 1
+
+   # Compare two expressions using if-elif
+x = 7
+y = 6
+
+if x > y:
+    print("x is greater than y")
+elif x < y:
+    print("x is less than y")
+else:
+    print("x is equal to y")
+
