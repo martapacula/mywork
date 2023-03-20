507e31dc588992fc8affa7118d1515c8c745e5eb
Marta Pa <G00425668@atu.ie>
2023-03-20 20:24:10 +0000

    week6

---
+++ week6/func.py
@@ -0,0 +1,14 @@
+#messing with functions
+#author Marta
+#let's define a function
+#x, y, z = (1, 2, 3)
+
+#def cube(number):
+    #print(number)
+
+#cube(333)
+import math
+number = float(input("Please input a positive number "))
+number1 = number / math.sqrt(number)
+print(f"{number1}")
+
